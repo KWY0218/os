8694b0f8655256104cdcfc9167beaef1c93229d6
GyuBa <sansam1010@naver.com>
2022-04-21 15:34:39 +0900

    #9 2nd commit : 주석추가

--- src/model/schedular/FCFS.java
+++ src/model/schedular/FCFS.java
@@ -12,6 +12,7 @@ public class FCFS implements Scheduler{
     public Queue<Process> running(Queue<Process> processList, int pCoreCount, int eCoreCount, int time) {
 //        System.out.println("FCFS Run");
         int counter = pCoreCount + eCoreCount;
+
         Queue<Process> resultList = new LinkedList<Process>();
         for(int i=0; (i < counter && !processList.isEmpty()); i++){
             resultList.add(processList.poll());
@@ -19,12 +20,6 @@ public class FCFS implements Scheduler{
         return resultList;
     }
 
-    /**
-     *
-     * @param runningProcess
-     * @param inProcess
-     * @return true : 현상유지, false: 변경
-     */
     @Override
     public boolean compareProcess(Process runningProcess, Process inProcess, int time) {
         return true;
--- src/model/schedular/SRTN.java
+++ src/model/schedular/SRTN.java
@@ -5,11 +5,26 @@ import model.process.Process;
 import java.util.Queue;
 
 public class SRTN implements Scheduler{
+    /**
+     * 실행시 현재 코어 개수만큼 processList에서 실행해야할 프로세스를 큐형태로 반환한다.
+     * @param processList 레디큐
+     * @param pCoreCount 작동 가능한 p코어 개수
+     * @param eCoreCount 작동 가능한 e코어 개수
+     * @param time 스케줄러 호출 시간
+     * @return 현재 실행해야할 코어 큐
+     */
     @Override
     public Queue<Process> running(Queue<Process> processList, int pCoreCount, int eCoreCount, int time) {
         return null;
     }
 
+    /**
+     * 기존에 실행중인 프로세스와 새로 할당할 프로세스를 비교하여, 기존 프로세스를 계속 처리할 경우 true를 할당된 프로세스를 변경시 false를 반환한다.
+     * @param runningProcess 기존에 실행중인 프로세스
+     * @param inProcess 새로 할당된 프로세스
+     * @param time 스케줄러 호출 시간
+     * @return true : 기존 프로세스 처리, false : 프로세스 교체
+     */
     @Override
     public boolean compareProcess(Process runningProcess, Process inProcess, int time) {
         return false;
